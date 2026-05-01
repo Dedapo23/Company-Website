7c8b6d95b9788a4c41b3d718ec8ca0996ddd4bf5
Dedapo23 <105419422+Dedapo23@users.noreply.github.com>
2026-05-01 19:08:36 +0100

    Create get-images.php

---
+++ get-images.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json');
+
+$galleryPath = 'gallery/';
+$images = [];
+
+if (is_dir($galleryPath)) {
+    $files = scandir($galleryPath);
+    foreach ($files as $file) {
+        $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+        if (in_array($extension, ['jpg', 'jpeg', 'png', 'gif', 'webp', 'bmp'])) {
+            $images[] = $file;
+        }
+    }
+}
+
+echo json_encode($images);
+?>
